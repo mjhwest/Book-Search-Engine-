d775b4bf5a0cc6f13dd77f3960fa0b6e6ca4c68c
mjhwest89 <mjhwest89@gmail.com>
2022-02-03 00:00:32 +1030

    finished working on typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,11 +8,10 @@ type Query {
     me: User
 } 
 
-
 type Mutation {
     login(email: String!, email: String!): Auth
     addUser(username:String!, email:String!, password:String!): Auth
-    saveBook(book: SaveBookInput): User
+    saveBook(input: SaveBookInput): User
     removeBook(bookId: String): User
 }
 
@@ -30,14 +29,27 @@ type User {
     _id: ID
  username: String!
  email: String!
- 
+ bookCount: Int
+ savedBooks: [Book]
+}
 
+type Book {
+    bookId: String! 
+    authors: [String]
+    description: String! 
+    title: String! 
+    image: String 
+    link: String 
+}
 
+type Auth { 
+    token: ID! 
+    user: User
 }
 
 `;
 
-
+module.exports = typeDefs;
 
 
 

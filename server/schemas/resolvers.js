8f91f90aef1514f3899d709882dac09df8fd0be7
mjhwest89 <mjhwest89@gmail.com>
2022-02-02 23:40:14 +1030

    more work to resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,13 +15,7 @@ const resolvers = {
       },
   },
 
-  //mutations are
-  //1 addUser - DONE 
-  //2 login - DONE 
-  //3 saveBook
-  //4 removeBook
   Mutation: {
-
     login: async (parent, {email, password }) => {
         const user = await User.findOne( {email });
       if (!user) {
@@ -34,21 +28,19 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     }, 
-
     addUser: async (parent, { username, email, password }) => {
         const user = await User.create({ username, email, password });
         const token = signToken(user);
         return { token, user };
       },
-
       //accept the 2nd argument as book which is an array, {book is an inpt type} 
-      saveBook: async (parent, { book }, context) => {
+      saveBook: async (parent, { input }, context) => {
         if (context.user) {
             const updatedUser = await User.findOneAndUpdate(
                 //match user ID 
                 { _id: context.user._id },
                 //add to set new book
-                { $addToSet: {savedBooks: book} },
+                { $addToSet: {savedBooks: input} },
                 //update new info new: true (NOT MAPPING)
                 { new: true }
             )
@@ -64,7 +56,7 @@ const resolvers = {
             { _id: context.user._id },
             //pull the matched bookId from saveBook 
             { $pull: { savedBooks: { bookId: bookId } } },
-            //update newInfo newL true
+            //update newInfo newL true, i.e. userId will have 1 less book
             { new: true }
           );
           //return updatedUser 
@@ -75,5 +67,4 @@ const resolvers = {
       },
     },
 };
-
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,54 @@
 const { gql } = require('apollo-server-express');
 
 
-const typeDefs = gql `
+const typeDefs = gql`
+
 
 type Query {
     me: User
-}, 
+} 
+
+
+type Mutation {
+    login(email: String!, email: String!): Auth
+    addUser(username:String!, email:String!, password:String!): Auth
+    saveBook(book: SaveBookInput): User
+    removeBook(bookId: String): User
+}
+
+
+input SavedBookInout {
+    author: [String]
+    description: String
+    bookId: String
+    image: String 
+    link: String
+    title: String
+}
+
+type User {
+    _id: ID
+ username: String!
+ email: String!
+ 
+
+
+}
 
 `;
+
+
+
+
+
+//mutation types  are linked to relationships with resolvers. 
+//i.e login requires email password, logging in awaits the User, therefore check 
+//user Model to type, i.e. email is a String, passworking is a string 
+//they are also both required, which means we use an ! 
+//and requires Auth to login 
+
+//mutation type for saveBook and remove book 
+//save book accepts BookAuthor array, description, title, bookId, image and link as params
+//thats why in resolvers we use {input} for 2nd param as it accepts the entite input
+//resolvers line 37, {input}
+//resolvers line 43, {addToSet: {savedBooks: input}} = adding the entire input to updated USeer. 

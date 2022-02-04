7eba236baadea11f71a333b609284ca89fcbcb95
mjhwest89 <mjhwest89@gmail.com>
2022-02-04 13:38:11 +1030

    push to save

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`🌍 Now listening on http://localhost:${PORT}`)
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
 });
